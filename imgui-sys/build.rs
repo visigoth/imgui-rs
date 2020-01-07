7a979f868c45adfc5a5c226e01f2168cee5edad9
Shaheen Gandhi <shaheen@oculus.com>
2020-01-07 15:49:11 -0800

    Set IMGUI_DISABLE_OBSOLETE_FUNCTIONS
    This is set by the cimgui build, so imgui-rs should set it as well.

--- imgui-sys/build.rs
+++ imgui-sys/build.rs
@@ -29,7 +29,8 @@ fn main() -> io::Result<()> {
     build
         .define("CIMGUI_NO_EXPORT", None)
         .define("IMGUI_DISABLE_WIN32_FUNCTIONS", None)
-        .define("IMGUI_DISABLE_OSX_FUNCTIONS", None);
+        .define("IMGUI_DISABLE_OSX_FUNCTIONS", None)
+        .define("IMGUI_DISABLE_OBSOLETE_FUNCTIONS", "1");
 
     build.flag_if_supported("-Wno-return-type-c-linkage");
     for path in &CPP_FILES {
